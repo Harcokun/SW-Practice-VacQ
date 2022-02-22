69d647469b44c41cf496653e65346378a3ca11dc
Harcokun <harconsarnan@gmail.com>
2022-02-22 20:24:04 +0700

    Add login function with token

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //Load env vars
@@ -10,13 +11,18 @@ connectDB();
 
 //Routes files
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 
 const app = express();
 
 //Body parser
 app.use(express.json());
 
-app.use('/api/v1/hospitals', hospitals)
+//Cookie parser
+app.use(cookieParser());
+
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
