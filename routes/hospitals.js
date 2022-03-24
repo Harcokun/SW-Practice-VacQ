e5c907d8ed765995adafa438bc123cae908a9831
Harcokun <harconsarnan@gmail.com>
2022-03-24 12:34:28 +0700

    add VacCenter API

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -11,8 +11,8 @@ const {protect, authorize} = require('../middleware/auth');
 //Re-route into other resource routers
 router.use('/:hospitalId/appointments/', appointmentRouter);
 
+router.route('/vacCenters').get(getVacCenters);
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
 router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
-router.route('/vacCenters').get(getVacCenters);
 
 module.exports = router;
